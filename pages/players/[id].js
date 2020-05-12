ad78d68b25ce6e4b4ce77feb53a80f33c2d7300c
chknwaffles <wonsae.kim@gmail.com>
2020-05-12 07:38:55 -0400

    fix id head

--- pages/players/[id].js
+++ pages/players/[id].js
@@ -99,7 +99,7 @@ export default function Player({ playerData }) {
     return (
         <Layout>
             <Head>
-                <title>{playerData.name} | 2KDB</title>
+                <title>{playerData.info.name} | 2KDB</title>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
             </Head>
             <div className="container is-fluid is-desktop">
