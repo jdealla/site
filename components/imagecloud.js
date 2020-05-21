cc9ef8cebd6c2b6e82e8ab5ba6aa7a79136a124d
chknwaffles <wonsae.kim@gmail.com>
2020-05-21 16:44:00 -0400

    fix imagecloud component

--- components/imagecloud.js
+++ components/imagecloud.js
@@ -1,16 +1,21 @@
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 import { Image, Transformation } from "cloudinary-react";
 
 const isProd = process.env.NODE_ENV === 'production'
 const BASE_URL = isProd ? `${process.env.VERCEL_URL}` : "http://localhost:3000"
 
 export default function ImageCloud(props) {
-  if (props.src == null)
-    return null;
+  const mounted = useRef(false)
 
-  return (
+  useEffect(() => {
+    mounted.current = true;
+
+    return () => { mounted.current = false; }
+  }, [])
+
+  return mounted.current ? (
       <Image cloudName={process.env.CLOUD_API_NAME} publicId={`${BASE_URL}/${props.src}`} type="fetch">
-        <Transformation width={props.width} height={props.height} gravity="faces" crop="limit" fetchFormat="auto" quality="75" />
-      </Image>
-  )
+        <Transformation width={props.width} height={props.height} gravity="faces" crop="limit" fetchFormat="auto" quality="auto" />
+      </Image> 
+  ) : null
 }
