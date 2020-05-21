2cd4baafd8ef92fe12fb212fa035d1160750256c
chknwaffles <wonsae.kim@gmail.com>
2020-05-21 15:15:14 -0400

    fix mounting issue

--- components/imagecloud.js
+++ components/imagecloud.js
@@ -5,13 +5,12 @@ const isProd = process.env.NODE_ENV === 'production'
 const BASE_URL = isProd ? `${process.env.VERCEL_URL}` : "http://localhost:3000"
 
 export default function ImageCloud(props) {
-  const imageRef = useRef();
+  if (props.src == null)
+    return null;
 
-  console.log(process.env.CLOUD_API_NAME);
-
-  return imageRef != null ? (
-      <Image innerRef={imageRef} cloudName={process.env.CLOUD_API_NAME} publicId={`${BASE_URL}/${props.src}`} type="fetch">
+  return (
+      <Image cloudName={process.env.CLOUD_API_NAME} publicId={`${BASE_URL}/${props.src}`} type="fetch">
         <Transformation width={props.width} height={props.height} gravity="faces" crop="limit" fetchFormat="auto" quality="75" />
       </Image>
-  ) : null;
+  )
 }
