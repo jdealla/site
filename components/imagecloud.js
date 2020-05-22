30aee934674c8ec33cd54db37680291a0016bb87
chknwaffles <wonsae.kim@gmail.com>
2020-05-21 22:23:08 -0400

    remove mounted ref and useeffect in imagecloud component

--- components/imagecloud.js
+++ components/imagecloud.js
@@ -2,17 +2,11 @@ import { useRef, useEffect } from "react";
 import { Image, Transformation } from "cloudinary-react";
 
 export default function ImageCloud(props) {
-  const mounted = useRef(false)
 
-  useEffect(() => {
-    mounted.current = true;
 
-    return () => { mounted.current = false; }
-  }, [])
-
-  return mounted.current ? (
+  return (
       <Image publicId={`${props.src}`} cloudName="dndoa4fno">
-        <Transformation width={props.width} height={props.height} fetchFormat="auto" quality="auto" />
+        <Transformation width={props.width} height={props.height} quality="60" crop="limit" />
       </Image> 
-  ) : null
+  )
 }
