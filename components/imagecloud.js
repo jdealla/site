ebfce8f21f218c81ba434465f6a2df339ad96333
chknwaffles <wonsae.kim@gmail.com>
2020-05-21 22:31:21 -0400

    update players.csv

--- components/imagecloud.js
+++ components/imagecloud.js
@@ -1,9 +1,6 @@
-import { useRef, useEffect } from "react";
 import { Image, Transformation } from "cloudinary-react";
 
 export default function ImageCloud(props) {
-
-
   return (
       <Image publicId={`${props.src}`} cloudName="dndoa4fno">
         <Transformation width={props.width} height={props.height} quality="60" crop="limit" />
