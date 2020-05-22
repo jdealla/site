f1f7f2dccb8a6480bd4a33b2e269404f4226cc25
chknwaffles <wonsae.kim@gmail.com>
2020-05-21 23:13:25 -0400

    add secure tag to imagecloud

--- components/imagecloud.js
+++ components/imagecloud.js
@@ -2,7 +2,7 @@ import { Image, Transformation } from "cloudinary-react";
 
 export default function ImageCloud(props) {
   return (
-      <Image publicId={`${props.src}`} cloudName="dndoa4fno">
+      <Image publicId={`${props.src}`} cloudName="dndoa4fno" secure="true">
         <Transformation width={props.width} height={props.height} quality="60" crop="limit" />
       </Image> 
   )
