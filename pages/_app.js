6462ac58ad3013876e8b5ac507ea3ec5af151152
chknwaffles <wonsae.kim@gmail.com>
2020-06-25 11:53:35 -0400

    fix google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,28 @@
-import React from "react";
-import Head from "next/head";
+import React, { useEffect } from "react";
+import Router from "next/router";
+import * as gtag from "../lib/gtag";
 
 import Layout from "../components/layout";
 import "../styles/global.scss"
 
-export default ({ Component, pageProps }) => (
-    <>
-        <Head>
-            <script
-                dangerouslySetInnerHTML={{
-                    __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                  })(window,document,'script','dataLayer','UA-169217612-1');
-                    `,
-                }}
-            />
-        </Head>
-        <noscript dangerouslySetInnerHTML={{__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=UA-169217612-1" height="0" width="0" style="display:none;visibility:hidden;"></iframe>`}} />
+export default function App({ Component, pageProps }) {
+    useEffect(() => {
+        const handleRouteChange = (url) => {
+            gtag.pageview(url)
+        }
+
+        Router.events.on('routeChangeComplete', handleRouteChange)
+        
+        return () => {
+            Router.events.off('routeChangeComplete', handleRouteChange)
+        }
+    }, [])
+  
+    return (
         <Layout>
             <Component {...pageProps} />
         </Layout>
-    </>
-)
+    )
+}
 
 
