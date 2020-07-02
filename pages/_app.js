55ba396fa3bf49156d464dfb480e91a26c031096
chknwaffles <wonsae.kim@gmail.com>
2020-07-02 04:56:13 -0400

    test min code

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,7 @@
-import React, { useEffect } from "react";
-import Router from "next/router";
-import * as gtag from "../lib/gtag";
-
 import "../styles/global.scss"
 
 export default function App({ Component, pageProps }) {
-    useEffect(() => {
-        const handleRouteChange = (url) => {
-            gtag.pageview(url)
-        }
-
-        Router.events.on('routeChangeComplete', handleRouteChange)
-        
-        return () => {
-            Router.events.off('routeChangeComplete', handleRouteChange)
-        }
-    }, [])
-
     return (
         <Component {...pageProps} />
     )
 }
-
-
