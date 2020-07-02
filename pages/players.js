7660b0d49c04cfcbe478eacbf1b209b307117c62
chknwaffles <wonsae.kim@gmail.com>
2020-07-02 06:07:38 -0400

    removem more again

--- pages/players.js
+++ pages/players.js
@@ -1,11 +1,10 @@
 import React from "react";
 import Head from "next/head";
-import { getAllPlayersWithAllStats, getAllAnimations } from "../lib/players";
-import { getTotalNumOfBadges } from "../lib/helpers"
+import { getAllPlayersWithAllStats } from "../lib/players";
 
 import Layout from "../components/layout";
 
-export default function Players({ players, allAnimations }) {
+export default function Players({ players }) {
     return (
         <Layout players={[]} searchOn={false}>
             <Head>
@@ -23,32 +22,9 @@ export default function Players({ players, allAnimations }) {
 export async function getStaticProps() {
     const players = await getAllPlayersWithAllStats().catch(console.error);
 
-    players.sort((a, b) => {
-        let aBadges = getTotalNumOfBadges(a);
-        let bBadges = getTotalNumOfBadges(b);
-
-        if (a.overall > b.overall) {
-            return -1;
-        } else if (a.overall === b.overall) {
-            if (aBadges.hof > bBadges.hof) {
-                return -1;
-            } else if (aBadges.hof === bBadges.hof) {
-                if (a.name > b.name)
-                    return 1;
-                else
-                    return -1;
-            }
-        } else {
-            return 1;
-        }
-    })
-
-    const allAnimations = getAllAnimations(players);
-
     return {
         props: {
             players,
-            allAnimations,
         }
     }
 }
