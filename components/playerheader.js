c6d481ad250a872afe25c46276d6b841ba7002af
chknwaffles <wonsae.kim@gmail.com>
2020-07-05 05:37:31 -0400

    fix duo partner link on id page

--- components/playerheader.js
+++ components/playerheader.js
@@ -163,7 +163,7 @@ export default function PlayerHeader(props) {
 										    <div className="has-text-centered">
 										        <TagOverall hasBorder={true} overall={duoPartner.overall} />
                                             </div>
-                                            <a href={`/player/${playerData.info.name.replace(/( |')/g, "_").toLowerCase()}/${duoPartner.id}`}>{duoPartner.name}</a>
+                                            <a href={`/player/${playerData.info.name.replace(/( |')/g, "-").toLowerCase()}/${duoPartner.id}`}>{duoPartner.name}</a>
                                             {renderDuo()}
                                         </div>
                                     </div>
