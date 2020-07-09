345225617631a79e0f31400074f4eacd7cbc79bf
KennyMcK69 <64959392+KennyMcK69@users.noreply.github.com>
2020-07-09 23:11:59 +0300

    delete button on hover in lineups

--- components/lineupview.js
+++ components/lineupview.js
@@ -56,11 +56,9 @@ export default function LineupView(props) {
                 )
             } else {
                 item = (
-                    <div className="column is-4-mobile" key={`starters-${slot}`}>
-                        <div style={{ position: "relative" }}>
-                            <a className="delete" aria-label="delete" style={{ position: "absolute", top: 0, zIndex: "2" }} onClick={() => handleClick(slot, null)}></a>
-                        </div>
-                        <figure className="image is-3by4">
+                    <div className="column is-4-mobile " key={`starters-${slot}`}>
+                        <figure className="image is-3by4 delete-hover">
+                            <a className="delete is-medium" aria-label="delete" onClick={() => handleClick(slot, null)}></a>
                             <img src={`https://2kdbimg.com/380/${player.name.replace(/( |')/g, "_").toLowerCase()}_${player.id}.jpg`} />
                         </figure>
                     </div>
@@ -98,10 +96,8 @@ export default function LineupView(props) {
             } else {
                 item = (
                     <div className="column is-3-mobile" key={`bench-${slot}`}>
-                        <div style={{ position: "relative" }}>
-                            <a className="delete" aria-label="delete" style={{ position: "absolute", top: 0, zIndex: "2" }} onClick={() => handleClick(slot, null)}></a>
-                        </div>
-                        <figure className="image is-3by4">
+                        <figure className="image is-3by4 delete-hover">
+                            <a className="delete is-medium" aria-label="delete" onClick={() => handleClick(slot, null)}></a>
                             <img key={player.id} src={`https://2kdbimg.com/380/${player.name.replace(/( |')/g, "_").toLowerCase()}_${player.id}.jpg`} />
                         </figure>
                     </div>
